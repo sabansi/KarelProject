8f767e4b56d37aa40d449ee50f0728263e021cea
S <s>
2023-10-01 15:49:05 +0400

    {"types":"C","runs":19}

--- Problem01.java
+++ Problem01.java
@@ -1,32 +1,34 @@
 import stanford.karel.Karel;
 public class Problem01 extends Karel{
 	public void run(){
-		while (leftIsClear()){
-		      putBeeperRow();
-		      turnForClearLeftRow();
-		      putBeeperRow();
-		      turnForClearRightRow();      
-		   }
-		   putBeeperRow();
+		while (true) {
+            // Check if there is a beeper on the current corner.
+            if (noBeepersPresent()) {
+                // If there's no beeper, put one.
+                putBeeper();
+            }
 
-		function turnForClearLeftRow() {
-		   turnLeft();
-		   move();
-		   turnLeft();
-		}
-
-		function turnForClearRightRow() {
-		   turnRight();
-		   move();
-		   turnRight();
-		}
-
-		function putBeeperRow() {
-		   while(frontIsClear()){ 
-		      putBeeper();
-		      move();
-		   }
-		   putBeeper();
-		}
+            // Move to the next corner in the current row.
+            if (frontIsClear()) {
+                move();
+            } else {
+                // If there's no clear path in the current row, move to the next row.
+                if (facingEast()) {
+                    turnLeft();
+                    move();
+                    turnLeft();
+                } else {
+                    turnRight();
+                    move();
+                    turnRight();
+                }
+            }
+            
+            // Check if Karel has finished filling the world with beepers.
+            if (frontIsBlocked() && facingNorth()) {
+                break;
+            }
+        }
+    }
 }
-}
+
