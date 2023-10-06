7505dca4c4ffbeabcbf35cfe83c619bc798d7355
S <s>
2023-10-07 03:59:48 +0400

    {"types":"C","runs":4}

--- Problem09.java
+++ Problem09.java
@@ -1,60 +1,110 @@
-import stanford.karel.Karel;
+import stanford.karel.*;
 
-public class Problem09 extends Karel {
+public class Problem09 extends SuperKarel {
+
+	@Override
 	public void run() {
+		moveToFirstOperand();
+		multiply();
+	}
+
+	private void moveToFirstOperand() {
 		move();
+	}
+
+	// pre: Karel at 2x1 facing east
+	private void multiply() {
 		while (beepersPresent()) {
 			pickBeeper();
-			move();
-			transferBeepers();
+			moveToSecondOperand();
+			copySecondOperand();
+			returnSecondOperand();
+			returnToFirstOperandFromSecond();
 		}
 	}
 	
-	// transfers beepers from third to fourth
-	private void transferBeepers() {
-		addBeepers();
-		moveTwice();
+	//pre: Karel at 3x1 facing east
+	//post: Karel at 2x1 facing east
+	private void returnToFirstOperandFromSecond() {
 		turnAround();
-		returnBeepers();
-		for (int i = 0; i < 3; i++) {
-			move();
-		}
-		turnAround();
-	}
-	
-	// adds beepers from third to fourth and fifth
-	private void addBeepers() {
-		while (beepersPresent()) {
-			pickBeeper();
-			move();
-			putBeeper();
-			move();
-			putBeeper();
-			turnAround();
-			moveTwice();
-			turnAround();
-		}
-	}
-	
-	// returns beepers from fifth to third
-	private void returnBeepers() {
-		while (beepersPresent()) {
-			pickBeeper();
-			moveTwice();
-			putBeeper();
-			turnAround();
-			moveTwice();
-			turnAround();
-		}
-	}
-	
-	private void turnAround() {
-		turnLeft();
-		turnLeft();
-	}
-	
-	private void moveTwice() {
 		move();
+		turnAround();
+	}
+
+	//pre: Karel at 3x1 facing east
+	//post: Karel at 3x1 facing east
+	private void returnSecondOperand() {
+		moveToCopyFromSecondOperand();
+		while (beepersPresent()) {
+			returnBeeperFromCopy();
+			returnToCopy();
+		}
+		returnToSecondOperandFromCopy();
+	}
+	
+	//pre: Karel at 5x1 facing west
+	//post: Karel at 3x1 east
+	private void returnToSecondOperandFromCopy() {
+		move();
+		move();
+		turnAround();
+	}
+
+	//pre: Karel at 3x1 facing east
+	//post: Karel at 5x1 facing west
+	private void moveToCopyFromSecondOperand() {
+		move();
+		move();
+		turnAround();
+	}
+
+	//pre: Karel at 3x1 facing west
+	//post: Karel at 5x1 facing west
+	private void returnToCopy() {
+		turnAround();
+		move();
+		move();
+		turnAround();
+	}
+
+	//pre: Karel at 5x1 facing west
+	//post: Karel at 3x1 facing west
+	private void returnBeeperFromCopy() {
+		pickBeeper();
+		move();
+		move();
+		putBeeper();
+	}
+
+	//pre: Karel at 3x1 facing east
+	//post: Karel at 3x1 facing east
+	private void copySecondOperand() {
+		while (beepersPresent()) {
+			pickBeeper();
+			moveToResult();
+			putBeeper();
+			moveToCopy();
+			putBeeper();
+			returnToSecondOperand();
+		}
+	}
+
+	private void returnToSecondOperand() {
+		turnAround();
+		move();
+		move();
+		turnAround();
+	}
+
+	private void moveToCopy() {
+		move();
+	}
+
+	private void moveToResult() {
+		move();
+	}
+
+	private void moveToSecondOperand() {
 		move();
 	}
 }
