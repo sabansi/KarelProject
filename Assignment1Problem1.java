1a58ff457d8e505c92ae85602bc9f348893c349d
S <s>
2023-10-07 22:10:28 +0400

    {"types":"C","runs":1}

--- Assignment1Problem1.java
+++ Assignment1Problem1.java
@@ -1,4 +1,22 @@
+import stanford.karel.Karel;
 
-public class Assignment1Problem1 {
-
+public class Assignment1Problem1 extends Karel{
+	public void run() {
+		findMagazine();
+		//takeMagazine();
+		//getMagazineAtHome();
+	}
+	//karel moves to the position where beeper is
+	private void findMagazine() {
+		move();
+		turnRight();
+		for(int i=0; i<3; i++) {
+		move();
+		}
+	}
+	private void turnRight() {
+		for(int i=0; i<3; i++) {
+			turnLeft();
+			}
+	}
 }
