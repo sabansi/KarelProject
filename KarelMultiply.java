9f589d32c3aaaf1cbf5c59045155e3cf2b8174b2
S <s>
2023-10-07 04:51:51 +0400

    {"types":"C","runs":11}

--- KarelMultiply.java
+++ KarelMultiply.java
@@ -1,4 +1,29 @@
+import stanford.karel.Karel;
 
-public class KarelMultiply {
-
+public class KarelMultiply extends Karel {
+	public void run() {
+		move();
+		while(beepersPresent()) {	
+			pickBeeper();
+			move();
+			while(beepersPresent()) {
+				pickBeeper();
+				addBeepersToForthAndFifthPiles();
+				turnAround();
+				move();
+				move();
+				turnAround();
+			}
+		}
+	}
+	public void addBeepersToForthAndFifthPiles() {
+		move();
+		putBeeper();
+		move();
+		putBeeper();
+	}
+	public void turnAround() {
+		turnLeft();
+		turnLeft();
+	}
 }
