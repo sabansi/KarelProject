5485456ae95ef3489491b614b755faf8a5d8dd7a
S <s>
2023-10-09 17:30:55 +0400

    {"types":"C","runs":11}

--- Problem12.java
+++ Problem12.java
@@ -5,6 +5,7 @@ public class Problem12 extends GraphicsProgram{
 	public void run() {
 		GOval myOval = new GOval(100, 200);
 		add(myOval, 100, 100);
-		add(myOval, 200, 200);
+		GOval secondOval = new GOval(100, 200);
+		add(secondOval, 200, 200);
 	}
 }
