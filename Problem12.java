0d653ccd71a5f766813ba50c55aad9a57596b03e
S <s>
2023-10-09 17:43:55 +0400

    {"types":"C","runs":20}

--- Problem12.java
+++ Problem12.java
@@ -7,6 +7,7 @@ public class Problem12 extends GraphicsProgram{
 	public void run() {
 		GOval myOval = new GOval(100, 200);
 		add(myOval, 100, 100);
+		myOval.setColor(Color.magenta);
 		//GOval secondOval = new GOval(100, 200);
 		myOval = new GOval(200, 200);
 		add(myOval, 200, 200);
