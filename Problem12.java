0a77609e3a904c730a43a15ee8f79bce5aa3e85b
S <s>
2023-10-10 11:15:39 +0400

    {"types":"C","runs":2}

--- Problem12.java
+++ Problem12.java
@@ -5,7 +5,7 @@ import acm.program.GraphicsProgram;
 
 public class Problem12 extends GraphicsProgram{
 	public void run() {
-		GOval myOval = new GOval(100, 200);
+		/* GOval myOval = new GOval(100, 200);
 		add(myOval, 100, 100);
 		myOval.setFilled(true);
 		myOval.setColor(Color.magenta);
@@ -13,6 +13,16 @@ public class Problem12 extends GraphicsProgram{
 		myOval = new GOval(200, 200);
 		add(myOval, 200, 200);
 		myOval.setFilled(true);
-		myOval.setColor(Color.PINK);
+		myOval.setColor(Color.PINK); 
+		*/
+		drawMidLine();
+	}
+	private void drawMidLine() {
+		double x1 = 0;
+		double y1 = getWidth();
+		double x2 = getHeight()/2;
+		double y2 = y1;
+		GLine Line = new GLine(x1, y1, x2, y2);
+		add(Line);
 	}
 }
