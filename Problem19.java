275f00e8ffdddcdf0b23138e1fd057f48004352f
S <s>
2023-10-11 16:31:07 +0400

    {"types":"C","runs":16}

--- Problem19.java
+++ Problem19.java
@@ -6,7 +6,7 @@ public class Problem19 extends ConsoleProgram {
 		int n = readInt("Enter the number: ");
 		//for loop to get through every number from 0 to n-1
 		for(int i = 0; i < n; i ++) {
-			println("Hello" + i);
+			println("Hello " + i);
 		}
 	}
 }
