907739ea8d596cfadba2c1b309929a95dcc7acd3
S <s>
2023-10-11 16:13:10 +0400

    {"types":"C","runs":8}

--- Problem16.java
+++ Problem16.java
@@ -5,6 +5,6 @@ public class Problem16 extends ConsoleProgram {
 		double a = readInt("Enter the first number: ");
 		double b = readInt("Enter the second number: ");
 		double c = readInt("Enter the third number: ");
-		println("the harmonic mean of these numbers is: " + (double) ( 1 / ( 1 / a + 1 / b + 1 / c ) ) );
+		println("The harmonic mean of these numbers is: " + (double) (1 / ( 1 / a + 1 / b + 1 / c )));
 	}
 }
--- Problem17.java
+++ Problem17.java
@@ -1,4 +1,8 @@
+import acm.program.ConsoleProgram;
 
-public class Problem17 {
-
+public class Problem17 extends ConsoleProgram {
+	public void run() {
+		double a = readInt("Enter the number: ");
+		println("The integer part of this number is: " + (int) a + " and the decimal part is: " + ( a - (int) a));
+	}
 }
