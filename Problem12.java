af3311e1a17e0c5f3276112e40ee441411173dea
S <s>
2023-10-11 04:18:36 +0400

    {"types":"C","runs":47}

--- Problem12.java
+++ Problem12.java
@@ -50,7 +50,7 @@ public class Problem12 extends GraphicsProgram {
 	
 	private void drawDoor() {
 		GRect wall = new GRect(DOOR_WIDTH, DOOR_HEIGHT);
-		int x = (int) (getWidth() / 2 - HOUSE_WIDTH / 2 + DOOR_WIDTH * 2);
+		int x = (int) (getWidth() / 2 - HOUSE_WIDTH / 2 + DOOR_WIDTH * 1.2);
 		int y = (int) (getHeight() - DOOR_HEIGHT);
 		add(wall, x, y);
 	}
