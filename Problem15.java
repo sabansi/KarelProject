f9c824ef4c04a8974309d1011402dd239436e4c2
S <s>
2023-10-11 15:53:34 +0400

    {"types":"C","runs":4}

--- Problem15.java
+++ Problem15.java
@@ -3,7 +3,7 @@ import acm.program.*;
 public class Problem15 extends ConsoleProgram {
 	public void run() {
 		double x = readInt("Enter the first number: ");
-		double y = readInt("Enter the first number: ");
+		double y = readInt("Enter the second number: ");
 		println("the arithmetic mean of these numbers is: " + (double) ((x + y) / 2));
 	}
 }
