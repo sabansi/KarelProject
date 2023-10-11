c56c76f926b92c31a8c99e15b6fbf54a2664c8ef
S <s>
2023-10-11 16:31:00 +0400

    {"types":"C","runs":15}

--- Problem19.java
+++ Problem19.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Problem19 {
-
+public class Problem19 extends ConsoleProgram {
+	public void run() {
+		//enter a number
+		int n = readInt("Enter the number: ");
+		//for loop to get through every number from 0 to n-1
+		for(int i = 0; i < n; i ++) {
+			println("Hello" + i);
+		}
+	}
 }
