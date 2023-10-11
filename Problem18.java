e005263c8df7ba7ad5f585508c37771bc8913c87
S <s>
2023-10-11 16:25:43 +0400

    {"types":"C","runs":13}

--- Problem18.java
+++ Problem18.java
@@ -1,4 +1,16 @@
+import acm.program.*;
 
-public class Problem18 {
-
+public class Problem18 extends ConsoleProgram {
+	public void run() {
+		//enter numbers
+		int x = readInt("Enter the first number: ");
+		int y = readInt("Enter the second number: ");
+		//compare numbers
+		if(x > y) {
+			println("maximum between these numbers is: " + x);
+		}
+		else {
+			println("maximum between these numbers is: " + y);
+		}
+	}
 }
