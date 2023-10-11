a80e9cd24ae3c0937b67a2346d0608397df8b999
S <s>
2023-10-11 15:48:06 +0400

    {"types":"C","runs":1}

--- Problem15.java
+++ Problem15.java
@@ -1,4 +1,9 @@
+import acm.program.*;
 
-public class Problem15 {
-
+public class Problem15 extends ConsoleProgram {
+	public void run() {
+		int x = readInt("Enter the first number: ");
+		int y = readInt("Enter the first number: ");
+		println("the arithmetic mean of these numbers is: " + ( x + y ) / 2);
+	}
 }
