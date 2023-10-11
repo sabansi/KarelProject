73736b3730ee5918b14efa633ba5e3754fb8b42a
S <s>
2023-10-11 04:27:42 +0400

    {"types":"C","runs":63}

--- Problem12.java
+++ Problem12.java
@@ -61,11 +61,15 @@ public class Problem12 extends GraphicsProgram {
 		GRect window1 = new GRect(WINDOWS_WIDTH, WINDOWS_HEIGHT);
 		int x1 = (int) (getWidth() / 2 - HOUSE_WIDTH / 2 + 20);
 		int y1 = (int) (getHeight() - DOOR_HEIGHT - 125);
+		window1.setFilled(true);
+		window1.setColor(Color.BLACK);
 		add(window1, x1, y1);
 		
 		GRect window2 = new GRect(WINDOWS_WIDTH, WINDOWS_HEIGHT);
 		int x2 = (int) (getWidth() / 2 - HOUSE_WIDTH / 2 + 40 + DOOR_WIDTH * 2);
 		int y2 = (int) (getHeight() - DOOR_HEIGHT - 125);
+		window2.setFilled(true);
+		window2.setColor(Color.BLACK);
 		add(window2, x2, y2);
 	}
 }
