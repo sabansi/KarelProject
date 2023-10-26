c30d108d611e2351854f57e115990e2fb3da1a9c
S <s>
2023-10-26 17:58:17 +0400

    {"types":"C","runs":35}

--- Problem31.java
+++ Problem31.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Problem31 {
-
+public class Problem31 extends ConsoleProgram{
+	public void run() {
+		int n = readInt("enter a numer: ");
+		if(checkPrime(n)) {
+			println(n + " is a prime number");
+		}
+		else {
+			println(n + " is a composed number");
+		}
+	}
+	private boolean checkPrime(int n) {
+		for(int i = 2; i <= Math.sqrt(n); i++) {
+			if(n % i == 0)
+				return false;
+		}
+		return true;
+	}
 }
