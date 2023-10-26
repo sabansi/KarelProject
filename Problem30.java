4e11663fe272c9620709b9c9f54ca57cdec8e5a8
S <s>
2023-10-26 17:43:22 +0400

    {"types":"C","runs":30}

--- Problem30.java
+++ Problem30.java
@@ -4,12 +4,16 @@ public class Problem30 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter a number: ");
 		int m = n;
+		println("the inversed number of " + m + " is : " + reverse(m));
+	}
+	private int reverse(int n) {
 		int ans = 0;
 		while(n > 0) {
 			ans +=  n % 10;
 			ans *= 10;
 			n /= 10;
 		}
-		println("the inversed number of " + m + " is : " + ans/10);
+		ans /= 10;
+		return ans;
 	}
 }
