a07e05b818f7c73b7a8bd0c679c939ddf278613b
S <s>
2023-10-26 17:10:23 +0400

    {"types":"C","runs":1}

--- Problem27.java
+++ Problem27.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class Problem27 {
-
+public class Problem27 extends ConsoleProgram { 
+	public void run() {
+		int n = readInt("Enter the first number: ");
+		int m = readInt("Enter the second number: ");
+		int ans = 1;
+		for(int i=0; i<m; i++) {
+			ans *= n;
+		}
+		println("n power of m is: " + ans);
+	}
 }
