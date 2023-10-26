3f261a2d9a0ed826239cd5efdc439e79534c082a
S <s>
2023-10-26 17:37:35 +0400

    {"types":"C","runs":22}

--- Problem29.java
+++ Problem29.java
@@ -3,14 +3,12 @@ import acm.program.ConsoleProgram;
 public class Problem29 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("enter a number: ");
-		if(n == 1 || n == 2) {
-			println("the " + n + "th number of fibonacci sequence is: 1");
-		}
-		else {
-			println("the " + n + "th number of fibonacci sequence is: " + fibonacciSeq(n));
-		}
+	    println("the " + n + "th number of fibonacci sequence is: " + fibonacciSeq(n));
 	}
 	private int fibonacciSeq(int n) {
+		if(n == 1 || n == 2) {
+			return 1;
+		}
 		int first = 1;
 		int second = 1;
 		int ans = 0;
