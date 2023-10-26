1bdbf8a8ad6796a55b9322012dbf860cb5cd4d2e
S <s>
2023-10-26 17:44:30 +0400

    {"types":"C","runs":32}

--- Problem27.java
+++ Problem27.java
@@ -4,10 +4,13 @@ public class Problem27 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter the first number: ");
 		int m = readInt("Enter the second number: ");
+		println("n power of m is: " + power(n, m));
+	}
+	private int power(int n, int m) {
 		int ans = 1;
 		for(int i=0; i<m; i++) {
 			ans *= n;
 		}
-		println("n power of m is: " + ans);
+		return ans;
 	}
 }
