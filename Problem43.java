7b49c77fb672dacd0765b6ea2ba91d3bb55a54e6
S <s>
2023-11-14 17:40:35 +0400

    {"types":"C","runs":27}

--- Problem43.java
+++ Problem43.java
@@ -1,10 +1,12 @@
+import acm.graphics.GOval;
 import acm.program.*;
 public class Problem43 extends GraphicsProgram{
 	private static final int RADIUS = 50;
 	private GOval circle;
+
 	public void run() {
 		circle = new GOval(2* RADIUS, 2 * RADIUS);
-		add(circle, getWIdth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
+		add(circle, getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
 		addMouseListeners();
 	}
 }
