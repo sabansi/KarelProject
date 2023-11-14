d05f5cb5a88cc2051b9c0e348dcf1e87c787bdf2
S <s>
2023-11-14 17:17:37 +0400

    {"types":"C","runs":20}

--- Problem41.java
+++ Problem41.java
@@ -21,14 +21,14 @@ public class Problem41 extends GraphicsProgram {
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		addBrush();
+		addBrush(e.getX(), e.getY());
 	}
 
-	private void addBrush() {
+	private void addBrush(int centerX, int centerY) {
 		GOval brush = new GOval(2 * RADIUS, 2 * RADIUS);
 		brush.setFilled(true);
-		int x = e.getX() - RADIUS;
-		int y = e.getY() - RADIUS;
+		int x = centerX - RADIUS;
+		int y = centerY - RADIUS;
 		add(brush, x, y);
 	}
 }
