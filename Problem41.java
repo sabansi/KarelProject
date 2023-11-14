8781d1f852f719d22e129dc29eb99488f63c5689
S <s>
2023-11-14 17:13:16 +0400

    {"types":"C","runs":12}

--- Problem41.java
+++ Problem41.java
@@ -1,4 +1,20 @@
+import java.awt.event.MouseEvent;
 
-public class Problem41 {
+import acm.graphics.GOval;
+import acm.program.*;
 
+public class Problem41 extends GraphicsProgram {
+	private static final int RADIUS = 20;
+	public void run() {
+		addMouseListeners();
+	}
+}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		GOval brush = new GOval(2 * RADIUS, 2 * RADIUS);
+		brush.setFilled(true);
+		int x = e.getX();;
+		int y = e.getY();
+		add(brush, x, y);
+	}
 }
