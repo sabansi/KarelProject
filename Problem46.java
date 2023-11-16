54d0f08b7def8ace1952839786151264b84cc29a
S <s>
2023-11-16 17:46:08 +0400

    {"types":"C","runs":15}

--- Problem46.java
+++ Problem46.java
@@ -7,7 +7,7 @@ public class Problem46 extends ConsoleProgram {
 		String input = readLine("Enter a word: ");
 		char answer = ' ';
 		for (int i = 0; i < input.length(); i++) {
-			for (int j = 0; j < input.length(); i++) {
+			for (int j = 0; j < input.length(); j++) {
 				if (input.charAt(i) == input.charAt(j) && i != j) {
 					countIndex++;
 				}
