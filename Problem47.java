7b6fb182c4b5a2028946e8f37f323eff2b4d067e
S <s>
2023-11-16 17:52:06 +0400

    {"types":"C","runs":28}

--- Problem47.java
+++ Problem47.java
@@ -4,15 +4,14 @@ public class Problem47 extends ConsoleProgram {
 	public void run() {
 		String input = readLine("Enter a word: ");
 		int count = 0;
-		for(int i = 0; i < input.length(); i++) {
-			if(input.charAt(i) == input.charAt(input.length() - i - 1)) {
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == input.charAt(input.length() - i - 1)) {
 				count++;
 			}
 		}
-		if(count == input.length()) {
+		if (count == input.length()) {
 			println("This word IS a palindrome!");
-		}
-		else {
+		} else {
 			println("This word is NOT a palindrome!");
 		}
 	}
