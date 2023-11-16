a1e1aef3371a381d0b8e3a888791027062195573
S <s>
2023-11-16 17:43:41 +0400

    {"types":"C","runs":12}

--- Problem45.java
+++ Problem45.java
@@ -1,12 +1,12 @@
 import acm.program.*;
 
-public class Problem45 extends ConsoleProgram{
+public class Problem45 extends ConsoleProgram {
 	public void run() {
 		int answer = 0;
 		String inputWord = readLine("Enter word: ");
 		String inputSymb = readLine("Enter symbol: ");
-		for(int i = 0; i < inputWord.length(); i++) {
-			if(inputWord.charAt(i) == inputSymb.charAt(0)) {
+		for (int i = 0; i < inputWord.length(); i++) {
+			if (inputWord.charAt(i) == inputSymb.charAt(0)) {
 				answer++;
 			}
 		}
