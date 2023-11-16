bc12dba7bf45e14d9221585943562f0a125a897e
S <s>
2023-11-16 17:45:54 +0400

    {"types":"C","runs":14}

--- Problem46.java
+++ Problem46.java
@@ -1,12 +1,11 @@
 import acm.program.*;
-import java.lang.Math;
 
 public class Problem46 extends ConsoleProgram {
 	public void run() {
 		int countMax = 0;
 		int countIndex = 0;
 		String input = readLine("Enter a word: ");
-		char answer = 0;
+		char answer = ' ';
 		for (int i = 0; i < input.length(); i++) {
 			for (int j = 0; j < input.length(); i++) {
 				if (input.charAt(i) == input.charAt(j) && i != j) {
