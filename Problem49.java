33e1d738fb1e6021a181e0786c236d359d997651
S <s>
2023-11-21 17:44:38 +0400

    {"types":"C","runs":3}

--- Problem49.java
+++ Problem49.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class Problem49 {
+public class Problem49 extends ConsoleProgram {
+	public void run() {
+		String input = readLine("Enter a word: ");
+		int num = stringToInt(input);
+		println(num);
+	}
 
+	private int stringToInt(String input) {
+		int num = 0;
+		for(int i = 0; i < input.length(); i++) {
+			char currCh = input.charAt(i);
+			int currNm = currCh - '0';
+			num = num * 10 + currNm;
+		}
+		return num;
+	}
 }
