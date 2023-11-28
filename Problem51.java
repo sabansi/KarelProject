dc6e4faf42d152f80c5351ebda06d9e0d7051add
S <s>
2023-11-28 17:39:46 +0400

    {"types":"C","runs":8}

--- Problem51.java
+++ Problem51.java
@@ -8,7 +8,16 @@ public class Problem51 extends ConsoleProgram{
 	}
 	
 	private String removeDuplicates(String text) {
-		println(text.replace("ba", "BA"));
-		return text;
+		String result = "";
+		for(int i = 0; i < text.length(); i++) {
+			result += text.charAt(i);
+			char ch = text.charAt(i);
+			for(int j = i; j < text.length(); j++) {
+				if(text.charAt(i) != text.charAt(j)) {
+					result += text.charAt(j);
+				}
+			}
+		}
+		return result;
 	}
 }
