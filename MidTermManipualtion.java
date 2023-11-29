181ecfcae1603ba310a94e48cf0d9aac5b1f4ce9
S <s>
2023-11-29 15:35:35 +0400

    {"types":"AR","runs":37}

---
+++ MidTermManipualtion.java
@@ -0,0 +1,32 @@
+import acm.program.ConsoleProgram;
+
+public class MidTermManipualtion extends ConsoleProgram {
+	public void run() {
+		String text1 = readLine("Enter first string: ");
+		String text2 = readLine("Enter second string: ");
+		if (checkSplit(text1, text2)) {
+			println("True");
+		} else {
+			println("False");
+		}
+	}
+
+	private boolean checkSplit(String text1, String text2) {
+		int count = 0;
+		int lastInd = -1;
+		for (int i = 0; i < text2.length(); i++) {
+			for (int j = lastInd + 1; j < text1.length(); j++) {
+				if (text2.charAt(i) == text1.charAt(j)) {
+					lastInd = j;
+					count++;
+					break;
+				}
+			}
+		}
+		if (count == text2.length()) {
+			return true;
+		}
+		return false;
+	}
+
+}
