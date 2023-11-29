e60fa94080112b36860f157eb030371ee192f453
S <s>
2023-11-29 15:41:28 +0400

    {"types":"C","runs":40}

--- MidTermManipualtion.java
+++ MidTermManipualtion.java
@@ -4,14 +4,15 @@ public class MidTermManipualtion extends ConsoleProgram {
 	public void run() {
 		String text1 = readLine("Enter first string: ");
 		String text2 = readLine("Enter second string: ");
-		if (checkSplit(text1, text2)) {
+	/*	if (checkSplit(text1, text2)) {
 			println("True");
 		} else {
 			println("False");
-		}
+		} */
+		checkSplit(text1, text2);
 	}
 
-	private boolean checkSplit(String text1, String text2) {
+	private void checkSplit(String text1, String text2) {
 		String splitText1 = "";
 		String splitText2 = "";
 		for(int i = 0; i < text1.length(); i++) {
@@ -21,6 +22,6 @@ public class MidTermManipualtion extends ConsoleProgram {
 			println(splitText2);
 		}
 		
-		return false;
+//		return false;
 	}
 }
