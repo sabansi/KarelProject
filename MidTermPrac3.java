f2649829d03934b4bbab78e79d0fc5da577fc9cc
S <s>
2023-11-29 15:08:17 +0400

    {"types":"C","runs":17}

--- MidTermPrac3.java
+++ MidTermPrac3.java
@@ -1,4 +1,32 @@
+import acm.program.ConsoleProgram;
 
-public class MidTermPrac3 {
+public class MidTermPrac3 extends ConsoleProgram {
+	public void run() {
+		String text1 = readLine("Enter first string: ");
+		String text2 = readLine("Enter second string: ");
+		if(checkSplit(text1, text2)) {
+			println("True");
+		}
+		else {
+			println("False");
+		}
+	}
 
+	private boolean checkSplit(String text1, String text2) {
+		int count = 0;
+		for(int i = 0; i < text2.length(); i++) {
+			for(int j = 0; j < text1.length(); j++) {
+				if(text2.charAt(i) == text1.charAt(j)) {
+					count++;
+					break;
+				}
+			}
+		}
+		if(count == text2.length()) {
+			return true;
+		}
+		return false;
+	}
+
+	
 }
