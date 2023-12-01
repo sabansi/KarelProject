4102b5cfbb55d48715bfcdb9de2fe12afe8e5116
S <s>
2023-12-01 16:25:36 +0400

    {"types":"C","runs":39}

--- MidTermPrac6.java
+++ MidTermPrac6.java
@@ -1,4 +1,22 @@
+import java.awt.event.MouseEvent;
 
-public class MidTermPrac6 {
+import acm.graphics.GLine;
+import acm.graphics.GPoint;
+import acm.program.GraphicsProgram;
 
+public class MidTermPrac6 extends GraphicsProgram {
+
+	private GPoint prevPoint;
+	private GLine line;
+
+	public void run() {
+		addMouseListeners();
+		prevPoint = new GPoint(0, 0);
+	}
+
+	public void mousePressed(MouseEvent e) {
+		line = new GLine(prevPoint.getX(), prevPoint.getY(), e.getX(), e.getY());
+		add(line);
+		prevPoint = new GPoint(e.getX(), e.getY());
+	}
 }
