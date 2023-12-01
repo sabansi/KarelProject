0016532bd7d1c7ea5eeb455b2a683dd5e6ae732e
S <s>
2023-12-01 21:27:28 +0400

    {"types":"C","runs":19}

--- leetcode1016.java
+++ leetcode1016.java
@@ -1,30 +1,35 @@
-
 import java.awt.Color;
-import java.awt.event.MouseEvent;
 
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
-import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class leetcode1016 extends GraphicsProgram {
-	private static final int BOX_SIZE = 80;
-	private GRect square;
-	private GLabel text;
-	private String score = "0";
-	public void run() {
-		draw();
-	}
+    private static final int BOX_SIZE = 80;
+    private GRect square;
+    private GLabel text;
+    private int score = 0;
 
-	private void draw() {
-		double x1 = getWidth() / 2 - BOX_SIZE / 2;
-		double y1 = getHeight() / 2 - BOX_SIZE / 2;
-		square = new GRect(x1, y1, BOX_SIZE, BOX_SIZE);
-		add(square);
-		text = new GLabel(score, getWidth() / 2 - text.getAscent() / 2, getHeight() / 2 - text.getHeight() / 2);
-		add(text);
-		
-	}
+    public void run() {
+        draw();
+    }
+
+    private void draw() {
+        double x1 = getWidth() / 2 - BOX_SIZE / 2;
+        double y1 = getHeight() / 2 - BOX_SIZE / 2;
+        square = new GRect(x1, y1, BOX_SIZE, BOX_SIZE);
+        add(square);
+
+        // Initialize the text label
+        updateScoreLabel();
+
+        waitForClick(); 
+    }
+
+    private void updateScoreLabel() {
+        text = new GLabel(Integer.toString(score), getWidth() / 2 - BOX_SIZE / 4, getHeight() / 2);
+        add(text);
+    }
 }
