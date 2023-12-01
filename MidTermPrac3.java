aea64b4eeaaa57581ae69115705d25d18a6e3bed
S <s>
2023-12-01 14:55:37 +0400

    {"types":"C","runs":17}

--- MidTermPrac3.java
+++ MidTermPrac3.java
@@ -12,21 +12,38 @@ public class MidTermPrac3 extends ConsoleProgram {
 	}
 
 	private boolean checkSplit(String text1, String text2) {
-		int count = 0;
-		int lastInd = -1;
-		for (int i = 0; i < text2.length(); i++) {
-			for (int j = lastInd + 1; j < text1.length(); j++) {
-				if (text2.charAt(i) == text1.charAt(j)) {
-					lastInd = j;
-					count++;
-					break;
+		for(int i = 1; i < text1.length() - 1; i++) {
+			String split1 = text1.substring(0, i);
+			String split2 = text1.substring(i);
+			if(manipulation(split1, split2, text2)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean manipulation(String split1, String split2, String text2) {
+		for(int i = 0; i < split1.length(); i++) {
+			String newSplit1 = removeChars(split1, split1.charAt(i));
+			for(int j = 0; j < split2.length(); j++) {
+				String newSplit2 = removeChars(split2, split2.charAt(j));
+				String result = newSplit1 + newSplit2;
+				if(result.equals(text2)) {
+					return true;
 				}
 			}
 		}
-		if (count == text2.length()) {
-			return true;
-		}
 		return false;
+	}
+
+	private String removeChars(String text, char ch) {
+		String removed = "";
+		for(int i = 0; i < text.length(); i++) {
+			if(text.charAt(i) == ch) {
+				text = text.replace(text.charAt(i) + "", " ");
+			}
+		}
+		return text;
 	}
 
 }
