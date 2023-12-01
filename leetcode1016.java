bcc297bf879204f4477e07d099c74f6e4a3a1142
S <s>
2023-12-01 21:26:28 +0400

    {"types":"C","runs":17}

--- leetcode1016.java
+++ leetcode1016.java
@@ -2,6 +2,7 @@
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
@@ -11,6 +12,8 @@ import acm.util.RandomGenerator;
 public class leetcode1016 extends GraphicsProgram {
 	private static final int BOX_SIZE = 80;
 	private GRect square;
+	private GLabel text;
+	private String score;
 	public void run() {
 		draw();
 	}
@@ -20,5 +23,8 @@ public class leetcode1016 extends GraphicsProgram {
 		double y1 = getHeight() / 2 - BOX_SIZE / 2;
 		square = new GRect(x1, y1, BOX_SIZE, BOX_SIZE);
 		add(square);
+		text = new GLabel(score, getWidth() / 2 - text.getAscent() / 2, getHeight() / 2 - text.getHeight() / 2);
+		add(text);
+		
 	}
 }
