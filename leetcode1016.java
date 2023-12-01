4cd9e9667e43e5e3c15c71074d35d9ce8cec8395
S <s>
2023-12-01 19:40:14 +0400

    {"types":"C","runs":18}

--- leetcode1016.java
+++ leetcode1016.java
@@ -6,59 +6,63 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class leetcode1016 extends GraphicsProgram {
-	
-	RandomGenerator rand = RandomGenerator.getInstance();
-	
-	private final static int CIRCLE_D = 40;
-	
-	private GOval circle;
-	private Color circleColor;
-	private Color currColor;
-	
-	public void run() {
-		drawCircle(CIRCLE_D / 2);
-		addMouseListeners();
-	}
+    
+    RandomGenerator rand = RandomGenerator.getInstance();
+    
+    private final static int CIRCLE_D = 40;
+    
+    private GOval circle;
+    private Color circleColor;
+    private boolean mousePressed = false;
 
-	private void drawCircle(double radius) {
-		double x = getWidth() / 2 - radius;
-		double y = getHeight() / 2 - radius;
-		circle = new GOval(x, y, radius * 2, radius * 2);
-		circleColor = rand.nextColor(); // Store the original color
-		circle.setFilled(true);
-		circle.setFillColor(circleColor);
-		add(circle);
-	}
-	
-	public void mouseClicked(MouseEvent e) {
-		circle.setFilled(true);
-		circle.setFillColor(rand.nextColor());
-	}
-	
-	public void mouseDragged(MouseEvent e) {
-		double x1 = e.getX();
-		double y1 = e.getY();
-		double x2 = getWidth() / 2;
-		double y2 = getHeight() / 2;
-		
-		currColor = rand.nextColor();
+    public void run() {
+        drawCircle(CIRCLE_D / 2);
+        addMouseListeners();
+    }
 
-		double newRadius = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    private void drawCircle(double radius) {
+        double x = getWidth() / 2 - radius;
+        double y = getHeight() / 2 - radius;
+        circle = new GOval(x, y, radius * 2, radius * 2);
+        circleColor = rand.nextColor(); // Store the original color
+        circle.setFilled(true);
+        circle.setFillColor(circleColor);
+        add(circle);
+    }
+    
+    public void mouseClicked(MouseEvent e) {
+        mousePressed = true;
+        circleColor = circle.getFillColor(); // Store the current color
+        circle.setFilled(true);
+        circle.setFillColor(rand.nextColor());
+    }
 
+    public void mouseReleased(MouseEvent e) {
+        mousePressed = false;
+    }
 
-		updateCircleRadius(newRadius);
-	}
+    public void mouseDragged(MouseEvent e) {
+        if (mousePressed) {
+            double x1 = e.getX();
+            double y1 = e.getY();
+            double x2 = getWidth() / 2;
+            double y2 = getHeight() / 2;
 
-	private void updateCircleRadius(double newRadius) {
-		double x = getWidth() / 2 - newRadius;
-		double y = getHeight() / 2 - newRadius;
+            double newRadius = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 
+            updateCircleRadius(newRadius);
+        }
+    }
 
-		remove(circle);
+    private void updateCircleRadius(double newRadius) {
+        double x = getWidth() / 2 - newRadius;
+        double y = getHeight() / 2 - newRadius;
 
-		circle = new GOval(x, y, newRadius * 2, newRadius * 2);
-		circle.setFilled(true);
-		circle.setFillColor(currColor);
-		add(circle);
-	}
+        remove(circle);
+
+        circle = new GOval(x, y, newRadius * 2, newRadius * 2);
+        circle.setFilled(true);
+        circle.setFillColor(circleColor); // Set the color to the stored color
+        add(circle);
+    }
 }
