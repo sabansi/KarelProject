d2a6edeaccb5a0d1b7997b353f4c788338a9bd84
S <s>
2023-12-01 16:51:39 +0400

    {"types":"C","runs":48}

--- MidTermPrac6.java
+++ MidTermPrac6.java
@@ -1,28 +1,29 @@
 import java.awt.event.MouseEvent;
 
-import acm.graphics.GLine;
-import acm.graphics.GPoint;
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class MidTermPrac6 extends GraphicsProgram {
-
-	private GPoint prevPoint;
-	private GLine line;
+	private static final int RADIUS = 50;
+	private static final int V_Y = 5;
+	private static final int DELAY = 20;
+	private GOval circle;
 
 	public void run() {
+		circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+
 		addMouseListeners();
-		prevPoint = new GPoint(0, 0);
+
+		// while loop should always be in run(not in listener methods)
+		while (true) {
+			circle.move(0, V_Y);
+			pause(DELAY);
+		}
 	}
 
-	public void mousePressed(MouseEvent e) {
-		line = new GLine(prevPoint.getX(), prevPoint.getY(), e.getX(), e.getY());
-		add(line);
-		prevPoint = new GPoint(e.getX(), e.getY());
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		add(circle, e.getX() - RADIUS, e.getY() - RADIUS);
 	}
-
-	public void mouseDragged(MouseEvent e) {
-		line.setEndPoint(e.getX(), e.getY());
-		prevPoint = new GPoint(e.getX(), e.getY());
-	}
-
 }
