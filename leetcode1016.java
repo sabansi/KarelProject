bdfe025c32dc68f139251b8bf5b95097552177c7
S <s>
2023-12-01 14:22:12 +0400

    {"types":"C","runs":2}

--- leetcode1016.java
+++ leetcode1016.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class leetcode1016 {
-
+public class leetcode1016 extends ConsoleProgram{
+	public void run() {
+		String numb = readLine();
+		int n = readInt();
+		if(binaryConvert(numb) == n) {
+			println("true");
+		}
+		else {
+			println("false");
+		}
+	}
+	private int binaryConvert(String numb) {
+		int binary = Integer.parseInt(numb);
+		return binary;
+	}
 }
