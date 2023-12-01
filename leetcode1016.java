019c0cdd7506f351219cdaeb50f225af165e0c72
S <s>
2023-12-01 19:13:35 +0400

    {"types":"C","runs":5}

--- leetcode1016.java
+++ leetcode1016.java
@@ -24,7 +24,7 @@ public class leetcode1016 extends GraphicsProgram {
 		add(circle);
 	}
 	
-	public void mouseClicked() {
+	public void mouseClicked(MouseEvent e) {
 		circle.setFilled(true);
 		circle.setFillColor(rand.nextColor());
 	}
