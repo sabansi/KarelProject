738786f1a2b6caff0635204a64b0e3c055ae920f
S <s>
2023-12-01 16:11:19 +0400

    {"types":"C","runs":28}

--- MidTermPrac5.java
+++ MidTermPrac5.java
@@ -12,19 +12,33 @@ public class MidTermPrac5 extends ConsoleProgram {
 
 	private boolean isMelodic(String s) {
 		String newStr = "";
+		int count1 = 0;
+		int count2 = 0;
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
 					|| s.charAt(i) == 'u' || s.charAt(i) == 'y') {
 				newStr += s.charAt(i);
 			}
 		}
-	/*	if (newStr.length() == 6) {
-			if (newStr.charAt(0) == 'a' && newStr.charAt(1) == 'e' && newStr.charAt(2) == 'i' && newStr.charAt(3) == 'o'
-					&& newStr.charAt(4) == 'u' && newStr.charAt(5) == 'y') {
-				return true;
+		if (newStr.length() == 6) {
+			for(int i = 0; i < newStr.length(); i++) {
+				if(newStr.charAt(i) < newStr.charAt(i+1)) {
+					count1++;
+				}
 			}
-		} */
-		println(newStr);
+			if(count1 == newStr.length() - 1) {
+				for(int i = 0; i < newStr.length(); i++) {
+					char ch = newStr.charAt(i);
+					if(newStr.indexOf(ch) == i) {
+						count2++;
+					}
+				}
+				if(count2 == count1 + 1) {
+					return true;
+				}
+			}
+		} 
+
 		return false;
 	}
 }
