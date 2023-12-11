27ad2f457c86f18dce7d048d6f3641dc014caf8e
S <s>
2023-12-11 18:00:52 +0400

    {"types":"A","runs":0}

---
+++ Problem50.java
@@ -0,0 +1,17 @@
+import java.util.StringTokenizer;
+
+import acm.program.ConsoleProgram;
+
+public class Problem50 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		int count = 0;
+		while (tokenizer.hasMoreTokens()) {
+			String currToken = tokenizer.nextToken();
+			count++;
+		}
+		println("Count: " + count);
+	}
+
+}
