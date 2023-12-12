f75a70a6d4cac1d82832d8247da8066c82eaa243
S <s>
2023-12-12 17:33:26 +0400

    {"types":"C","runs":1}

--- Problem60.java
+++ Problem60.java
@@ -6,7 +6,7 @@ public class Problem60 extends ConsoleProgram{
 	public void run(){
 		int n = 0;
 		ArrayList<Integer> arr = new ArrayList<Integer>;
-		while(n != -1) {
+		while(n = (readInt("Enter a number:")) != -1) {
 			n = (readInt("Enter a number:"));
 			arr.add(n);
 		}
