11e083de1ec4d2481df23909cc85d4fd07c31d91
S <s>
2023-12-14 17:17:26 +0400

    {"types":"C","runs":0}

--- Problem58.java
+++ Problem58.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class Problem58 {
+public class Problem58 extends ConsoleProgram {
+	public void run() {
 
+	}
+
+	private boolean isMagicMatrix(int[][] mat) {
+		int n = mat.length;
+		int m = mat[0].length;
+		boolean occ[] = null;
+		if (n == m) {
+			for (int i = 1; i <= n; i++) {
+				{
+					for (int j = 1; j <= n; j++) {
+						occ[mat[i][j]] = true;
+					}
+				}
+			}
+			for (int i = 1; i <= n * n; i++) {
+				if (occ[i] == false)
+					return false;
+			}
+			return true;
+		}
+		return false;
+	}
 }
