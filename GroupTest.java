99f2c03b53fc38e30ca9d82b706bd5626dd9296b
S <s>
2024-01-09 17:36:23 +0400

    {"types":"C","runs":1}

--- GroupTest.java
+++ GroupTest.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
 
-public class GroupTest {
+import acm.program.ConsoleProgram;
+
+public class GroupTest extends ConsoleProgram{
+	public void run() { 
+		ArrayList<String> students = new ArrayList();
+		students.add("sansi23");
+		students.add("akave23");
+		Group classB = new Group(students);
+		println(classB.getScore("sansi23"));
+		println(classB.getScore("akave22"));
+	}
 
 }
