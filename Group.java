41600dead7e4d147a0b7cf123a531e9cbda1aec0
S <s>
2024-01-09 17:42:41 +0400

    {"types":"C","runs":4}

--- Group.java
+++ Group.java
@@ -4,36 +4,38 @@ import java.util.Map;
 
 public class Group {
 	private Map<String, Double> studentScores;
+
 	public Group() {
 		studentScores = new HashMap<String, Double>();
 	}
-	
+
 	public Group(ArrayList<String> students) {
 		studentScores = new HashMap<>();
-		for(String student: students) {
+		for (String student : students) {
 			studentScores.put(student, null);
 		}
 	}
-	
+
 	public Double getScore(String student) {
-		if(!studentScores.containsKey(student)) {
+		if (!studentScores.containsKey(student)) {
 			System.out.println("No such student: " + student);
 			return null;
 		}
 		return studentScores.get(student);
 	}
-	
+
 	public void setScore(String student, double score) {
-		if(!studentScores.containsKey(student)) {
+		if (!studentScores.containsKey(student)) {
 			System.out.println("No such student: " + student);
 			return;
 		}
 		studentScores.put(student, score);
 	}
-	
+
 	public void addStudent(String student) {
-		if(!studentScores.containsKey(student))
-		studentScores.put(student, null);
+		if (!studentScores.containsKey(student)) {
+			studentScores.put(student, null);
+		}
 	}
 
 }
