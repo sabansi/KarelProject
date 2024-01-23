681e189bf1fe879ee37ca35a8652798d6333b2b1
S <s>
2024-01-23 22:11:56 +0400

    {"types":"C","runs":1}

--- same.java
+++ same.java
@@ -1,4 +1,22 @@
+import java.util.HashMap;
 
-public class same {
+import acm.program.ConsoleProgram;
 
+public class same extends ConsoleProgram{
+	public void run() {
+		String s1 = readLine("Enter first string: ");
+		String s2 = readLine("Enter second string: ");
+		String s = s1 + s2;
+		HashMap<Character, Integer> map = new HashMap<>();
+		for(char ch : s.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
+		}
+		for(char ch : map.keySet()) {
+			if(map.get(ch) % 2 != 0) {
+				println("false");
+				break;
+			}
+		}
+		println("true");
+	}
 }
