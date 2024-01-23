3eefa82604fd3238e2ce75d97e21d703319c7689
S <s>
2024-01-23 22:12:46 +0400

    {"types":"C","runs":2}

--- same.java
+++ same.java
@@ -2,21 +2,25 @@ import java.util.HashMap;
 
 import acm.program.ConsoleProgram;
 
-public class same extends ConsoleProgram{
+public class same extends ConsoleProgram {
 	public void run() {
 		String s1 = readLine("Enter first string: ");
 		String s2 = readLine("Enter second string: ");
 		String s = s1 + s2;
 		HashMap<Character, Integer> map = new HashMap<>();
-		for(char ch : s.toCharArray()) {
+		for (char ch : s.toCharArray()) {
 			map.put(ch, map.getOrDefault(ch, 0) + 1);
 		}
-		for(char ch : map.keySet()) {
-			if(map.get(ch) % 2 != 0) {
+		boolean check = true;
+		for (char ch : map.keySet()) {
+			if (map.get(ch) % 2 != 0) {
+				check = false;
 				println("false");
 				break;
 			}
 		}
-		println("true");
+		if (!check) {
+			println("true");
+		}
 	}
 }
