e89f763c90e2e6e8e08596ae10ce175c30e0b158
S <s>
2024-01-23 22:52:44 +0400

    {"types":"AR","runs":9}

---
+++ array2D.java
@@ -0,0 +1,20 @@
+public class array2D {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();  // Move to the next line after each row
+        }
+    }
+}
