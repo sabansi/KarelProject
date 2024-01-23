f8e46755a6ac316de4538fa63302e6d4e853d79c
S <s>
2024-01-23 16:27:55 +0400

    {"types":"C","runs":38}

--- casino.java
+++ casino.java
@@ -2,55 +2,52 @@ import acm.program.*;
 import acm.util.RandomGenerator;
 
 public class casino extends ConsoleProgram {
-    private RandomGenerator rgen = RandomGenerator.getInstance();
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 
-    public void run() {
-        int n = readInt("Enter the amount of money: ");
-        simulations(n);
-    }
+	public void run() {
+		int n = readInt("Enter the amount of money: ");
+		simulations(n);
+	}
 
-    private void simulations(int n) {
-        int totalRolls = 0;
+	private void simulations(int n) {
+		int totalRolls = 0;
 
-        for (int i = 0; i < 1; i++) {
-            int rolls = 0;
-            double currMoney = n;
-            double lostMoney = 0;
+		for (int i = 0; i < 1000; i++) {
+			int rolls = 0;
+			double currMoney = n;
+			double lostMoney = 0;
 
-            while (currMoney > 0) {
-                double betAmount = determineBetAmount(currMoney, lostMoney);
-                int roulette = rgen.nextInt(0, 36);
-                int winner = rgen.nextInt(0, 36);
+			while (currMoney > 0) {
+				double betAmount = determineBetAmount(currMoney, lostMoney);
+				int roulette = rgen.nextInt(0, 36);
+				int winner = rgen.nextInt(0, 36);
 
-                if (roulette == winner) {
-                    double winAmount = betAmount * 36.0;
-                    currMoney += winAmount;
-                    lostMoney -= winAmount;
-                } else {
-                    currMoney -= betAmount;
-                    lostMoney += betAmount;
-                }
+				if (roulette == winner) {
+					double winAmount = betAmount * 36.0;
+					currMoney += winAmount;
+					lostMoney -= winAmount;
+				} else {
+					currMoney -= betAmount;
+					lostMoney += betAmount;
+				}
 
-                println(betAmount);
-                println(currMoney);
-                println(lostMoney);
-                rolls++;
-            }
+				rolls++;
+			}
 
-            totalRolls += rolls;
-        }
+			totalRolls += rolls;
+		}
 
-        double averageRolls = (double) totalRolls / 1000;
-        println("Average number of rolls until losing all money: " + averageRolls);
-    }
+		double averageRolls = (double) totalRolls / 1000;
+		println("Average number of rolls until losing all money: " + averageRolls);
+	}
 
-    private double determineBetAmount(double currMoney, double lostMoney) {
-        if (lostMoney <= 0) {
-            return 1;
-        } else if (currMoney >= (lostMoney + 1) / 36.0) {
-            return ((lostMoney + 1) / 36.0);
-        } else {
-            return currMoney;
-        }
-    }
+	private double determineBetAmount(double currMoney, double lostMoney) {
+		if (lostMoney <= 0) {
+			return 1;
+		} else if (currMoney >= (lostMoney + 1) / 36.0) {
+			return ((lostMoney + 1) / 36.0);
+		} else {
+			return currMoney;
+		}
+	}
 }
