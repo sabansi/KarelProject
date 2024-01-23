d86510dd5752e23b23b15152a4f6112e968da07b
S <s>
2024-01-23 22:50:03 +0400

    {"types":"C","runs":6}

--- array2D.java
+++ array2D.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
 
-public class array2D {
+import acm.program.ConsoleProgram;
 
+public class array2D extends ConsoleProgram {
+	public void run() {
+		int[] input1 = { 1, 2 };
+		int[] input2 = { 2, 1, 1, 2 };
+
+		generate2DArray(input1);
+		generate2DArray(input2);
+	}
+
+	private void generate2DArray(int[] input) {
+		int[][] result = new int[input.length][];
+		for (int i = 0; i < input.length; i++) {
+			result[i] = new int[input[i]];
+		}
+		println(result);
+	}
 }
