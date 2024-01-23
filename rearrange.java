5319dd6922d4fc1dc71473f01ec3091a978600c2
S <s>
2024-01-23 15:27:46 +0400

    {"types":"C","runs":7}

--- rearrange.java
+++ rearrange.java
@@ -15,15 +15,27 @@ public class rearrange extends ConsoleProgram {
 	}
 
 	private void rearrange(String word) {
-		ArrayList< Pair<Integer, String> > pair = new ArrayList< Pair<Integer, String> >();
+		ArrayList<Pair<Integer, String>> pair = new ArrayList<Pair<Integer, String>>();
 		StringTokenizer tokenizer = new StringTokenizer(word, " ");
 		while (tokenizer.hasMoreTokens()) {
 			String token = tokenizer.nextToken();
 			pair.add(new Pair<>(token.length(), token));
 		}
 		
-		 Collections.sort(pair, Comparator.comparing(Pair::getKey));
-		for(Pair<Integer, String> curPair : pair) {
+		for(int i = 0; i < pair.size() - 1; i++) {
+			int minIndex = i;
+			for(int j = i + 1; j < pair.size(); j++) {
+				if(pair.get(j).getKey() < pair.get(minIndex).getKey()) {
+					minIndex = j;
+				}
+			}
+			Pair<Integer, String> temp = pair.get(i);
+	        pair.set(i, pair.get(minIndex));
+	        pair.set(minIndex, temp);
+		}
+
+	//	Collections.sort(pair, Comparator.comparing(Pair::getKey));
+		for (Pair<Integer, String> curPair : pair) {
 			print(curPair.getValue() + " ");
 		}
 	}
