fc293b94a7ba699c6d69de42a62271c291b5ac5f
S <s>
2024-01-23 16:00:46 +0400

    {"types":"C","runs":18}

--- casino.java
+++ casino.java
@@ -2,8 +2,9 @@ import java.util.*;
 import acm.program.*;
 import acm.util.RandomGenerator;
 
-public class casino extends ConsoleProgram{
+public class casino extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
+
 	public void run() {
 		int n = readInt("Enter the amount of money: ");
 		simulations(n);
@@ -11,25 +12,34 @@ public class casino extends ConsoleProgram{
 
 	private void simulations(int n) {
 		int middle = 0;
-		for(int ind = 0; ind < 1000; ind++) {
+		double currMoney = n;
+		for (int ind = 0; ind < 1000; ind++) {
 			double lostMoney = 0;
 			int rolls = 0;
-			while(n > 0) {
+			double betMoney = 0;
+			while (currMoney > 0) {
 				int roulette = rgen.nextInt(0, 36);
 				int winner = rgen.nextInt(0, 36);
-				double betMoney = (lostMoney + 1) / 36.0;
-				if(roulette == winner) {
-					n += lostMoney + 1;
+				if(currMoney >= n) {
+					betMoney = 1;
 				}
-				else {
+				else if (currMoney >= (lostMoney + 1) / 36.0) {
+					betMoney = (lostMoney + 1) / 36.0;
+				} else {
+					betMoney = currMoney;
+				}
+				if (roulette == winner) {
+					currMoney -= betMoney;
+					currMoney += lostMoney + 1;
+				} else {
 					lostMoney += betMoney;
-					n -= lostMoney;
+					currMoney -= betMoney;
 				}
 				rolls++;
 			}
 			middle += rolls;
 		}
 		double ans = middle / 1000;
-		println(middle);
+		println(ans);
 	}
 }
